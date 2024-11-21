3cb5a27118facd60501320c6a11a87bf0e2cdad9
ming <zzzzz124987@gmail.com>
2024-11-21 11:51:02 +0800

    [Hank] test

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
   base: './',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
