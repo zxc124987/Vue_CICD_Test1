874419a277e32a1d3c7bcc6ffe4a824e875c4563
ming <zzzzz124987@gmail.com>
2024-11-26 11:06:56 +0800

    [Hank] 調整

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Vue_CICD_Test1',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
